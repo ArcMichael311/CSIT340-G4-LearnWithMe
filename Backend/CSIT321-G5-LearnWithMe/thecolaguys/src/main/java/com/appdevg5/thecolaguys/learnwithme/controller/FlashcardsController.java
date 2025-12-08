aaa6feec4eac3db9527a5a2158a41fb38cf22a08
ArcMichael311 <your-email@example.com>
2025-12-09 06:59:15 +0800

    checking progress

--- Backend/CSIT321-G5-LearnWithMe/thecolaguys/src/main/java/com/appdevg5/thecolaguys/learnwithme/controller/FlashcardsController.java
+++ Backend/CSIT321-G5-LearnWithMe/thecolaguys/src/main/java/com/appdevg5/thecolaguys/learnwithme/controller/FlashcardsController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/flashcards")
+@CrossOrigin(origins = "http://localhost:3000")
 public class FlashcardsController {
 
     private final FlashcardsService flashcardsService;
