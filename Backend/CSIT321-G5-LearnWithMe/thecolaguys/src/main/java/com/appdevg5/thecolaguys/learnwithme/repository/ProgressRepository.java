b836a7f5cb6bc977f5f59d2d6117e62c9f30f402
ArcMichael311 <your-email@example.com>
2025-12-09 16:29:01 +0800

    Progress Repository fixing

--- Backend/CSIT321-G5-LearnWithMe/thecolaguys/src/main/java/com/appdevg5/thecolaguys/learnwithme/repository/ProgressRepository.java
+++ Backend/CSIT321-G5-LearnWithMe/thecolaguys/src/main/java/com/appdevg5/thecolaguys/learnwithme/repository/ProgressRepository.java
@@ -2,17 +2,22 @@ package com.appdevg5.thecolaguys.learnwithme.repository;
 
 import com.appdevg5.thecolaguys.learnwithme.entity.ProgressEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProgressRepository extends JpaRepository<ProgressEntity, Long> {
 
 	List<ProgressEntity> findByUserId(Long userId);
 
-	List<ProgressEntity> findByCardId(Long cardId);
-
 	List<ProgressEntity> findByDeckId(Long deckId);
+
+	Optional<ProgressEntity> findByUserIdAndDeckId(Long userId, Long deckId);
+
+	@Query("SELECT p FROM ProgressEntity p WHERE p.userId = :userId ORDER BY p.studyDate DESC")
+	List<ProgressEntity> findByUserIdOrderByDateDesc(Long userId);
 
 }
