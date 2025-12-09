b7fbe3bd67cb24779f8a6aadf0e25cdac1306d66
ArcMichael311 <your-email@example.com>
2025-12-09 13:57:50 +0800

    Deck Categories Repositorry fix delete button

--- Backend/CSIT321-G5-LearnWithMe/thecolaguys/src/main/java/com/appdevg5/thecolaguys/learnwithme/repository/Deck_CategoriesRepository.java
+++ Backend/CSIT321-G5-LearnWithMe/thecolaguys/src/main/java/com/appdevg5/thecolaguys/learnwithme/repository/Deck_CategoriesRepository.java
@@ -3,7 +3,10 @@ package com.appdevg5.thecolaguys.learnwithme.repository;
 import com.appdevg5.thecolaguys.learnwithme.entity.Deck_CategoriesEntity;
 import com.appdevg5.thecolaguys.learnwithme.entity.Deck_CategoriesId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,5 +20,10 @@ public interface Deck_CategoriesRepository extends JpaRepository<Deck_Categories
     void deleteByDeckIdAndCategoryId(Long deckId, Long categoryId);
     
     boolean existsByDeckIdAndCategoryId(Long deckId, Long categoryId);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Deck_CategoriesEntity d WHERE d.deckId = :deckId")
+    void deleteByDeckId(Long deckId);
 }
 
